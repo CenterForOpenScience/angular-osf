2256219431a9eb8488bf00f92b19bff5349583f7
Kyrylo Petrov <kpetrov@exoft.net>
2025-02-28 12:52:28 +0200

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SidenavComponent } from '@osf/sidenav/sidenav.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '@osf/header/header.component';
+import { MainContentComponent } from '@osf/main-content/main-content.component';
+import { FooterComponent } from '@osf/footer/footer.component';
+import { TopnavComponent } from '@osf/topnav/topnav.component';
 
 @Component({
   selector: 'osf-root',
-  imports: [RouterOutlet],
+  imports: [
+    SidenavComponent,
+    RouterOutlet,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent,
+    TopnavComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
@@ -11,4 +23,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'osf';
+  isDesktop = window.innerWidth > 1024;
 }
