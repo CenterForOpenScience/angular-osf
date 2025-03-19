88d995c58405ef5256d0f2e5a8364bca8b242b28
Roma <nikitinroma2605@gmail.com>
2025-03-19 13:49:14 +0200

    feat(developer-apps): Added create app dialog

--- src/app/features/settings/developer-apps/developer-apps-container.component.ts
+++ src/app/features/settings/developer-apps/developer-apps-container.component.ts
@@ -1,16 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
+import { DialogService } from 'primeng/dynamicdialog';
+import { CreateDeveloperAppComponent } from '@osf/features/settings/developer-apps/create-developer-app/create-developer-app.component';
 
 @Component({
   selector: 'osf-developer-apps',
   imports: [RouterOutlet, SubHeaderComponent],
   templateUrl: './developer-apps-container.component.html',
   styleUrl: './developer-apps-container.component.scss',
+  providers: [DialogService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeveloperAppsContainerComponent {
-  onCreateDeveloperAppBtnClicked(): void {
-    //TODO integrate API
+  private readonly dialogService = inject(DialogService);
+
+  createDeveloperApp(): void {
+    this.dialogService.open(CreateDeveloperAppComponent, {
+      width: '448px',
+      focusOnShow: false,
+      header: 'Create Developer App',
+      closeOnEscape: true,
+      modal: true,
+      closable: true,
+    });
   }
 }
