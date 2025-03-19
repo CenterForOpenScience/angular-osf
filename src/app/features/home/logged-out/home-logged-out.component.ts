ddc3c3ffc5558b28c5b2d871a24a52f5a810d0e2
Kyrylo Petrov <kpetrov@exoft.net>
2025-03-19 13:33:14 +0200

    fix(home page): separate files for hardcoded data

--- src/app/features/home/logged-out/home-logged-out.component.ts
+++ src/app/features/home/logged-out/home-logged-out.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { InputText } from 'primeng/inputtext';
 import { NgOptimizedImage } from '@angular/common';
+import { slides } from './slides';
 
 @Component({
   selector: 'osf-home-logged-out',
@@ -15,30 +16,5 @@ import { NgOptimizedImage } from '@angular/common';
 export class HomeLoggedOutComponent {
   searchValue = signal('');
 
-  slides = [
-    {
-      img: 'assets/images/carousel1.png',
-      name: '2-st slide',
-      title:
-        'OSF is a game changer for those wanting to effectively share their research process in the spirit of collaboration.',
-      author: 'Patricia Ayala',
-      facility: 'Research Services Librarian | University of Toronto',
-    },
-    {
-      img: 'assets/images/carousel2.png',
-      name: '1-st slide',
-      title:
-        'OSF is indispensable in helping me create reproducible research pipelines from preregistration through data collection and analysis. Its versatility makes it my one-stop shop for projects. The Dropbox integration effortlessly transforms my existing local workflow to public repository.',
-      author: 'Maya Mathur',
-      facility: 'Department of Epidemiology Harvard University',
-    },
-    {
-      img: 'assets/images/carousel3.png',
-      name: '1-st slide',
-      title:
-        'Because SocArXiv is a not-for-profit organization, researchers can be assured that they are sharing their research in an environment where access, inclusivity, and preservation, rather than profit, will remain at the heart of the mission. A great benefit of partnering with OSF is that this application is a free public good.',
-      author: 'Philip Cohen',
-      facility: 'SocArXiv papers',
-    },
-  ];
+  slides = slides;
 }
