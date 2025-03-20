2be36b7aa2d778479615e237344193fc500a307d
Kyrylo Petrov <kpetrov@exoft.net>
2025-03-20 16:48:46 +0200

    chore(profile-settings): name tab design
    # Conflicts:
    #	src/assets/styles/styles.scss

--- src/app/features/settings/profile-settings/profile-settings.component.ts
+++ src/app/features/settings/profile-settings/profile-settings.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'osf-profile-settings',
-  imports: [SubHeaderComponent],
+  imports: [
+    SubHeaderComponent,
+    TabList,
+    Tabs,
+    Tab,
+    TabPanel,
+    TabPanels,
+    Button,
+  ],
   templateUrl: './profile-settings.component.html',
   styleUrl: './profile-settings.component.scss',
 })
-export class ProfileSettingsComponent {}
+export class ProfileSettingsComponent {
+  defaultTabValue = 0;
+}
