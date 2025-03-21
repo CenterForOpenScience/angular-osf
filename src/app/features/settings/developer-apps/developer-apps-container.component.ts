9dce87d85dbf5162ce57bfc3dd4af8777c06e2fc
Roma <nikitinroma2605@gmail.com>
2025-03-21 11:35:42 +0200

    feat(developer-apps): Adjusted create developer app dialog width for different layouts

--- src/app/features/settings/developer-apps/developer-apps-container.component.ts
+++ src/app/features/settings/developer-apps/developer-apps-container.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { SubHeaderComponent } from '@shared/components/sub-header/sub-header.component';
 import { DialogService } from 'primeng/dynamicdialog';
 import { CreateDeveloperAppComponent } from '@osf/features/settings/developer-apps/create-developer-app/create-developer-app.component';
+import { IS_MEDIUM, IS_XSMALL } from '@shared/utils/breakpoints.tokens';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'osf-developer-apps',
@@ -14,10 +16,21 @@ import { CreateDeveloperAppComponent } from '@osf/features/settings/developer-ap
 })
 export class DeveloperAppsContainerComponent {
   private readonly dialogService = inject(DialogService);
+  isXSmall$ = inject(IS_XSMALL);
+  isMedium$ = inject(IS_MEDIUM);
+  isXSmall = toSignal(this.isXSmall$);
+  isMedium = toSignal(this.isMedium$);
 
   createDeveloperApp(): void {
+    let dialogWidth = '850px';
+    if (this.isXSmall()) {
+      dialogWidth = '345px';
+    } else if (this.isMedium()) {
+      dialogWidth = '500px';
+    }
+
     this.dialogService.open(CreateDeveloperAppComponent, {
-      width: '448px',
+      width: dialogWidth,
       focusOnShow: false,
       header: 'Create Developer App',
       closeOnEscape: true,
