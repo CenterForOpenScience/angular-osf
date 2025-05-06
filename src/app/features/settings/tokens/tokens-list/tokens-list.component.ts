fb6de3ea33ee3589e4dba44ba64779fea0b3ac8a
Roma <nikitinroma2605@gmail.com>
2025-05-06 13:20:52 +0300

    feat(pat): added skeleton when loading list

--- src/app/features/settings/tokens/tokens-list/tokens-list.component.ts
+++ src/app/features/settings/tokens/tokens-list/tokens-list.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   inject,
   OnInit,
+  signal,
 } from '@angular/core';
 import { ConfirmationService } from 'primeng/api';
 import { IS_XSMALL } from '@shared/utils/breakpoints.tokens';
@@ -14,10 +15,11 @@ import { Token } from '@osf/features/settings/tokens/entities/tokens.models';
 import { defaultConfirmationConfig } from '@shared/helpers/default-confirmation-config.helper';
 import { Store } from '@ngxs/store';
 import { DeleteToken, GetTokens, TokensSelectors } from '@core/store/settings';
+import { Skeleton } from 'primeng/skeleton';
 
 @Component({
   selector: 'osf-tokens-list',
-  imports: [Button, Card, RouterLink],
+  imports: [Button, Card, RouterLink, Skeleton],
   templateUrl: './tokens-list.component.html',
   styleUrl: './tokens-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -26,6 +28,7 @@ export class TokensListComponent implements OnInit {
   #store = inject(Store);
   #confirmationService = inject(ConfirmationService);
   #isXSmall$ = inject(IS_XSMALL);
+  protected readonly isLoading = signal(false);
   protected readonly isXSmall = toSignal(this.#isXSmall$);
 
   tokens = this.#store.selectSignal(TokensSelectors.getTokens);
@@ -49,7 +52,15 @@ export class TokensListComponent implements OnInit {
 
   ngOnInit(): void {
     if (!this.tokens().length) {
-      this.#store.dispatch(GetTokens);
+      this.isLoading.set(true);
+      this.#store.dispatch(GetTokens).subscribe({
+        complete: () => {
+          this.isLoading.set(false);
+        },
+        error: () => {
+          this.isLoading.set(false);
+        },
+      });
     }
   }
 }
