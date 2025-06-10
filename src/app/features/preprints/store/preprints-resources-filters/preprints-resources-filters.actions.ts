70cdccd122d64c2cc9d76f0d0e147b1a693607ed
Roma <nikitinroma2605@gmail.com>
2025-06-10 16:10:41 +0300

    feat(preprints-resources-filters-state): Implemented state

---
+++ src/app/features/preprints/store/preprints-resources-filters/preprints-resources-filters.actions.ts
@@ -0,0 +1,45 @@
+export class SetCreator {
+  static readonly type = '[Preprints Resource Filters] Set Creator';
+
+  constructor(
+    public name: string,
+    public id: string
+  ) {}
+}
+
+export class SetDateCreated {
+  static readonly type = '[Preprints Resource Filters] Set DateCreated';
+
+  constructor(public date: string) {}
+}
+
+export class SetSubject {
+  static readonly type = '[Preprints Resource Filters] Set Subject';
+
+  constructor(
+    public subject: string,
+    public id: string
+  ) {}
+}
+
+export class SetLicense {
+  static readonly type = '[Preprints Resource Filters] Set License';
+
+  constructor(
+    public license: string,
+    public id: string
+  ) {}
+}
+
+export class SetProvider {
+  static readonly type = '[Preprints Resource Filters] Set Provider';
+
+  constructor(
+    public provider: string,
+    public id: string
+  ) {}
+}
+
+export class ResetFiltersState {
+  static readonly type = '[Preprints Resource Filters] Reset State';
+}
