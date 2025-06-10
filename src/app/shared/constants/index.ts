52ff47df702ec9a44f3478c5f58a9e6e99f4684a
Roma <nikitinroma2605@gmail.com>
2025-06-10 17:17:45 +0300

    fix(shared-const-index): Fixed index file

--- src/app/shared/constants/index.ts
+++ src/app/shared/constants/index.ts
@@ -2,4 +2,5 @@ export * from './input-limits.const';
 export * from './input-validation-messages.const';
 export * from './remove-nullable.const';
 export * from './resource-filters-defaults';
+export * from './scientists.const';
 export * from './search-state-defaults.const';
