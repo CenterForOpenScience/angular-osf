13e082d11908d024fbf8a5ec7acd672faeda6200
Roma <nikitinroma2605@gmail.com>
2025-07-03 11:43:36 +0300

    refactor(preprints-state): Renamed Preprints -> PreprintProviders

--- src/app/features/preprints/store/preprint-providers/preprint-providers.actions.ts
+++ src/app/features/preprints/store/preprint-providers/preprint-providers.actions.ts
@@ -1,19 +1,19 @@
 export class GetPreprintProviderById {
-  static readonly type = '[Preprints] Get Provider By Id';
+  static readonly type = '[Preprint Providers] Get Provider By Id';
 
   constructor(public id: string) {}
 }
 
 export class GetHighlightedSubjectsByProviderId {
-  static readonly type = '[Preprints] Get Highlighted Subjects By Provider Id';
+  static readonly type = '[Preprint Providers] Get Highlighted Subjects By Provider Id';
 
   constructor(public providerId: string) {}
 }
 
 export class GetPreprintProvidersToAdvertise {
-  static readonly type = '[Preprints] Get Preprint Providers To Advertise';
+  static readonly type = '[Preprint Providers] Get Preprint Providers To Advertise';
 }
 
 export class GetPreprintProvidersAllowingSubmissions {
-  static readonly type = '[Preprints] Get Preprint Providers That Allows Submissions';
+  static readonly type = '[Preprint Providers] Get Preprint Providers That Allows Submissions';
 }
