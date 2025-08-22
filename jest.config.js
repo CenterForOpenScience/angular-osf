8e3fd3c1a14bf141d84c5aae3ac7e0f13f9abf46
Brian Pilati <brianpilati@cos.io>
2025-08-22 11:43:18 -0500

    chore(jest): enabled configure-addon.commponent.tst to be testable

--- jest.config.js
+++ jest.config.js
@@ -77,7 +77,6 @@ module.exports = {
     '<rootDir>/src/app/features/files/',
     '<rootDir>/src/app/features/my-projects/',
     '<rootDir>/src/app/features/preprints/',
-    '<rootDir>/src/app/features/project/addons/',
     '<rootDir>/src/app/features/project/analytics/',
     '<rootDir>/src/app/features/project/contributors/',
     '<rootDir>/src/app/features/project/files/',
