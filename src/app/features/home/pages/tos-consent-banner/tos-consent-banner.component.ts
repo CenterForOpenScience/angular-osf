16bfc29cbfd313e4bccad3cb68176af4e985a83f
mkovalua <mkoval@exoft.net>
2025-09-11 10:45:33 +0300

    implement tos banner component

--- src/app/features/home/pages/tos-consent-banner/tos-consent-banner.component.ts
+++ src/app/features/home/pages/tos-consent-banner/tos-consent-banner.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NgIf, CommonModule } from '@angular/common'; // <-- Import NgIf
 import { FormsModule } from '@angular/forms'; // <-- 1. Import this
+import { Button, ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { MessageModule } from 'primeng/message';
+import {createDispatchMap} from '@ngxs/store';
+import {GetCurrentUserSettings, UpdateUserSettings, GetCurrentUser} from '@core/store/user';
+import {
+  GetAllGlobalNotificationSubscriptions,
+  UpdateNotificationSubscription
+} from '@osf/features/settings/notifications/store';
 
 
 @Component({
-  selector: 'app-tos-consent-banner',
-  imports: [NgIf, FormsModule],
+  selector: 'cos-tos-consent-banner',
+  imports: [NgIf, FormsModule, CheckboxModule, ButtonModule ],
   templateUrl: './tos-consent-banner.component.html',
   styleUrls: ['./tos-consent-banner.component.scss'],
 })
@@ -21,7 +30,17 @@ export class TosConsentBannerComponent {
 
   errorMessage: string | null = null;
 
+  // private readonly actions = createDispatchMap({
+  //   getCurrentUserSettings: GetCurrentUserSettings,
+  //   getCurrentUser: GetCurrentUser,
+  //   getAllGlobalNotificationSubscriptions: GetAllGlobalNotificationSubscriptions,
+  //   updateUserSettings: UpdateUserSettings,
+  //   updateNotificationSubscription: UpdateNotificationSubscription,
+  // });
+
   onContinue() {
+    // alert(JSON.stringify(this.actions.GetCurrentUser()));
+
     if (!this.acceptedTermsOfService) {
       this.errorMessage = 'You must agree before continuing.';
       return;
