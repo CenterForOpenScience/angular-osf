b126c0fbaeaf9bac37e7c1898f5211aa77d4b397
Brian Pilati <brianpilati@cos.io>
2025-09-18 17:02:11 -0500

    chore(update-tests): updated tests and had to skip tests to

--- src/app/features/home/pages/dashboard/dashboard.component.ts
+++ src/app/features/home/pages/dashboard/dashboard.component.ts
@@ -14,6 +14,7 @@ import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
+import { ENVIRONMENT } from '@core/provider/environment.provider';
 import { CreateProjectDialogComponent } from '@osf/features/my-projects/components';
 import {
   IconComponent,
@@ -53,6 +54,7 @@ export class DashboardComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly translateService = inject(TranslateService);
   private readonly dialogService = inject(DialogService);
+  private readonly environment = inject(ENVIRONMENT);
   private readonly projectRedirectDialogService = inject(ProjectRedirectDialogService);
 
   readonly isMedium = toSignal(inject(IS_MEDIUM));
