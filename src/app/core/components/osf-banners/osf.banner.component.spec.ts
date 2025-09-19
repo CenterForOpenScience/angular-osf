d6cce9f8ffebd748f3ddd7e72b05aa40ae18a1db
Brian Pilati <brianpilati@cos.io>
2025-09-19 09:28:04 -0500

    chore(renames): renamed files part 2

---
+++ src/app/core/components/osf-banners/osf.banner.component.spec.ts
@@ -0,0 +1,33 @@
+import { MockComponent } from 'ng-mocks';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ScheduledBannerComponent } from './scheduled-banner/scheduled.banner.component';
+import { OSFBannerComponent } from './osf.banner.component';
+
+import { OSFTestingModule } from '@testing/osf.testing.module';
+import { MockComponentWithSignal } from '@testing/providers/component-provider.mock';
+
+describe('Component: OSF Banner', () => {
+  let fixture: ComponentFixture<OSFBannerComponent>;
+  let component: OSFBannerComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        OSFTestingModule,
+        OSFBannerComponent,
+        NoopAnimationsModule,
+        MockComponentWithSignal('osf-maintenance-banner'),
+        MockComponent(ScheduledBannerComponent),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(OSFBannerComponent);
+    component = fixture.componentInstance;
+  });
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+});
