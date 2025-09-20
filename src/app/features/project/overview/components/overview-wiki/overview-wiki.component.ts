da9286fba64ae1ef0b5f8b00cb84f33e1a234959
nsemets <nsemets@exoft.net>
2025-09-20 22:16:53 +0300

    fix(wiki): fixed wiki bugs

--- src/app/features/project/overview/components/overview-wiki/overview-wiki.component.ts
+++ src/app/features/project/overview/components/overview-wiki/overview-wiki.component.ts
@@ -4,14 +4,14 @@ import { TranslatePipe } from '@ngx-translate/core';
 
 import { Skeleton } from 'primeng/skeleton';
 
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
-import { MarkdownComponent, TruncatedTextComponent } from '@osf/shared/components';
+import { TruncatedTextComponent } from '@osf/shared/components';
 import { WikiSelectors } from '@osf/shared/stores';
 
 @Component({
-  selector: 'osf-project-wiki',
-  imports: [Skeleton, TranslatePipe, TruncatedTextComponent, MarkdownComponent],
+  selector: 'osf-overview-wiki',
+  imports: [Skeleton, TranslatePipe, TruncatedTextComponent],
   templateUrl: './overview-wiki.component.html',
   styleUrl: './overview-wiki.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +19,8 @@ import { WikiSelectors } from '@osf/shared/stores';
 export class OverviewWikiComponent {
   isWikiLoading = select(WikiSelectors.getHomeWikiLoading);
   wikiContent = select(WikiSelectors.getHomeWikiContent);
+
+  resourceId = input('');
+
+  wikiLink = () => ['/', this.resourceId(), 'wiki'];
 }
