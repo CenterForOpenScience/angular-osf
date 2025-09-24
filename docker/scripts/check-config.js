d33c3ffd87530cfadf28e619a587a31a8783311c
Oleh Paduchak <opaduchak@exoft.net>
2025-09-24 15:48:57 +0300

    fix(check-config): fixed check-config script

--- docker/scripts/check-config.js
+++ docker/scripts/check-config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const configPath = path.join(__dirname, '../src/assets/config/config.json');
-const templatePath = path.join(__dirname, '../src/assets/config/template.json');
+const configPath = path.join(__dirname, '../../src/assets/config/config.json');
+const templatePath = path.join(__dirname, '../../src/assets/config/template.json');
 
 if (!fs.existsSync(configPath)) {
   console.log('[INFO] config.json not found. Copying from template.json...');
