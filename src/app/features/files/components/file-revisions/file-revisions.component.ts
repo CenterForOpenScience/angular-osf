3dc901b2ec3fbc360c2cefc7a4ac994bca822df3
nsemets <nsemets@exoft.net>
2025-09-24 19:56:15 +0300

    fix(files): fixed files bugs

--- src/app/features/files/components/file-revisions/file-revisions.component.ts
+++ src/app/features/files/components/file-revisions/file-revisions.component.ts
@@ -1,5 +1,3 @@
-import { select } from '@ngxs/store';
-
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'primeng/accordion';
@@ -8,14 +6,12 @@ import { Skeleton } from 'primeng/skeleton';
 
 import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
 
 import { CopyButtonComponent } from '@osf/shared/components';
 import { InfoIconComponent } from '@osf/shared/components/info-icon/info-icon.component';
 import { StopPropagationDirective } from '@osf/shared/directives';
 
 import { OsfFileRevision } from '../../models';
-import { FilesSelectors } from '../../store';
 
 @Component({
   selector: 'osf-file-revisions',
@@ -38,12 +34,10 @@ import { FilesSelectors } from '../../store';
 })
 export class FileRevisionsComponent {
   fileRevisions = input<OsfFileRevision[] | null>();
+  isLoading = input(false);
 
   downloadRevision = output<string>();
   openRevision = output<string>();
-
-  readonly isLoading = select(FilesSelectors.isFileRevisionsLoading);
-  readonly resourceMetadata = toObservable(select(FilesSelectors.getResourceMetadata));
 
   onOpenRevision(version: string): void {
     this.openRevision.emit(version);
