5a7a9bbc04c58afef9aa997bb348e2aa6e69c8f0
mkovalua <mkoval@exoft.net>
2025-09-29 17:53:00 +0300

    fix(social share preview): fix formatting

--- src/app/shared/constants/social-share.config.ts
+++ src/app/shared/constants/social-share.config.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_SHARE_URLS = {
   email: 'mailto:',
-  twitter: {preview_url: 'https://twitter.com/intent/tweet', viaHandle: 'OsfFramework'},
+  twitter: { preview_url: 'https://twitter.com/intent/tweet', viaHandle: 'OsfFramework' },
   facebook: 'https://www.facebook.com/sharer/sharer.php',
   linkedIn: 'https://www.linkedin.com/shareArticle',
 };
