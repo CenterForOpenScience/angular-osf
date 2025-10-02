3fcbc741a5837d20e1e56191ed15cd00adf40b4f
mkovalua <mkoval@exoft.net>
2025-10-02 13:57:09 +0300

    feature(post metrics): code updates

--- src/app/shared/services/analytics.service.ts
+++ src/app/shared/services/analytics.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+
 import { ENVIRONMENT } from '@core/provider/environment.provider';
 
 @Injectable({ providedIn: 'root' })
@@ -7,8 +8,8 @@ export class AnalyticsService {
   private readonly http: HttpClient = inject(HttpClient);
   private readonly environment = inject(ENVIRONMENT);
 
-  get apiDomainUrl() {
-    return `${this.environment.apiDomainUrl}/_/metrics/events/counted_usage`;
+  apiDomainUrl() {
+    return `${this.environment.apiDomainUrl}/_/metrics/events/counted_usage/`;
   }
 
   async sendCountedUsage(guid: string, routeName: string): Promise<void> {
@@ -31,8 +32,7 @@ export class AnalyticsService {
     const headers = new HttpHeaders({
       'Content-Type': 'application/vnd.api+json',
     });
-
-    this.http.post(this.apiDomainUrl, payload, { headers }).subscribe({
+    this.http.post(this.apiDomainUrl(), payload, { headers }).subscribe({
       next: () => console.log('Usage event sent', payload),
       error: (err) => console.error('Error sending usage event', err),
     });
