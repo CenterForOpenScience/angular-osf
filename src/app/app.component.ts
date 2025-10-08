b7243ba5939564e14a648ac9cc92c7528c6642d3
mkovalua <mkoval@exoft.net>
2025-10-08 15:39:46 +0300

    fix( add email/merge account ): show model with merge warning if acc already exists and add alternative email modal if not exists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,10 +62,13 @@ export class AppComponent implements OnInit {
   }
 
   private showEmailDialog() {
+    const unverifiedEmailsData = this.unverifiedEmails();
     this.customDialogService.open(ConfirmEmailComponent, {
-      header: 'home.confirmEmail.title',
+      header: unverifiedEmailsData[0].isMerge
+        ? 'home.confirmEmail.isMerge.title'
+        : 'home.confirmEmail.isNotMerge.title',
       width: '448px',
-      data: this.unverifiedEmails(),
+      data: unverifiedEmailsData,
     });
   }
 }
