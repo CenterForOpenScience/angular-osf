8bf4632f4d9e60230c321d112aa98a051ac8cdfd
nsemets <nsemets@exoft.net>
2025-11-24 18:24:59 +0200

    fix(ssr): setup ssr

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppComponent } from '@osf/app.component';
 import { appConfig } from '@osf/app.config';
 
 bootstrapApplication(AppComponent, {
-  providers: [...appConfig.providers],
+  providers: [...appConfig.providers, provideClientHydration(withEventReplay())],
 }).catch((err) =>
   // eslint-disable-next-line no-console
   console.error(err)
