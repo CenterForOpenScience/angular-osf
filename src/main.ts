08efaad68fbaa63e4d0c9d9de1d8ca7cb77086d2
nsemets <nsemets@exoft.net>
2025-12-11 16:50:50 +0200

    fix(cedar): removed import

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppComponent } from '@osf/app.component';
 import { appConfig } from '@osf/app.config';
 
 import 'cedar-embeddable-editor';
-import 'cedar-artifact-viewer';
 
 bootstrapApplication(AppComponent, {
   providers: [...appConfig.providers],
