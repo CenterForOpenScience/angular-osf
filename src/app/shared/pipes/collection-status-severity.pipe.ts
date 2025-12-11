ffb2abe149bb9faa7afb142927143782e0322186
nsemets <nsemets@exoft.net>
2025-12-11 17:54:18 +0200

    [ENG-9728] Collection fixes (#818)
    * fix(styles): fixed styles
    
    * fix(collections): fixed status for collection

--- src/app/shared/pipes/collection-status-severity.pipe.ts
+++ src/app/shared/pipes/collection-status-severity.pipe.ts
@@ -2,13 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import { CollectionSubmissionReviewState } from '../enums/collection-submission-review-state.enum';
 import { COLLECTION_SUBMISSION_STATUS_SEVERITY } from '../helpers/collection-submission-status.util';
+import { CustomOption } from '../models/select-option.model';
 import { SeverityType } from '../models/severity.type';
 
 @Pipe({
   name: 'collectionStatusSeverity',
 })
 export class CollectionStatusSeverityPipe implements PipeTransform {
-  transform(status: CollectionSubmissionReviewState): SeverityType {
+  transform(status: CollectionSubmissionReviewState): CustomOption<SeverityType> {
     return COLLECTION_SUBMISSION_STATUS_SEVERITY[status];
   }
 }
