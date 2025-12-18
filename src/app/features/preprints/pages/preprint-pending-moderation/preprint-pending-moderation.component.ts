30dfcd19d65e0fa3337c92f8d7e7779f7a5e95ec
mkovalua <mkoval@exoft.net>
2025-12-18 22:59:30 +0200

    update preprint-pending-moderation info component styles

--- src/app/features/preprints/pages/preprint-pending-moderation/preprint-pending-moderation.component.ts
+++ src/app/features/preprints/pages/preprint-pending-moderation/preprint-pending-moderation.component.ts
@@ -1,10 +1,7 @@
-import { TranslatePipe } from '@ngx-translate/core';
-
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'osf-preprint-pending-moderation',
-  imports: [TranslatePipe],
   templateUrl: './preprint-pending-moderation.component.html',
   styleUrl: './preprint-pending-moderation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
