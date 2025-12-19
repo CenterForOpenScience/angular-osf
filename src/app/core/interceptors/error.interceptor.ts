b83cf04d7a4940a146f14484dbdc632ad5f7eac0
mkovalua <mkoval@exoft.net>
2025-12-19 12:09:44 +0200

    resolve linter issues

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -64,7 +64,7 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       if (error.status === 403) {
         const preprintIsUnderModeration = 'This preprint is pending moderation and is not yet publicly available.';
         if (errorMessage === preprintIsUnderModeration) {
-          const afterDomain =`/preprints/${document.URL.split('preprints')[1]}`;
+          const afterDomain = `/preprints/${document.URL.split('preprints')[1]}`;
           router.navigate([afterDomain + '/pending-moderation']);
         } else {
           const requestAccessRegex = /\/v2\/nodes\/[^/]+\/requests\/?$/i;
