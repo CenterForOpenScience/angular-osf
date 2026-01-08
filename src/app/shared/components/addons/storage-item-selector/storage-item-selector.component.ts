5903e47b34c494f03d9bbba9d2323bdba04c1a2b
Andriy Sheredko <sheredko.andriy@gmail.com>
2026-01-08 22:54:28 +0100

    fix(addons): Fix GitLab pagination Load More button not showing

--- src/app/shared/components/addons/storage-item-selector/storage-item-selector.component.ts
+++ src/app/shared/components/addons/storage-item-selector/storage-item-selector.component.ts
@@ -193,10 +193,10 @@ export class StorageItemSelectorComponent implements OnInit {
 
   readonly showLoadMoreButton = computed(() => {
     const invocation = this.operationInvocation();
-    if (!invocation?.nextSampleCursor || !invocation?.thisSampleCursor) {
+    if (!invocation?.nextSampleCursor) {
       return false;
     }
-    return invocation.nextSampleCursor > invocation.thisSampleCursor;
+    return invocation.nextSampleCursor !== invocation.thisSampleCursor;
   });
 
   handleCreateOperationInvocation(
