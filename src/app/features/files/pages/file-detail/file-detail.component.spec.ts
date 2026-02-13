b86b954848084e08ee5daf9c47adae22b30742ef
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2026-02-13 10:49:05 -0500

    fix(signposting): fix linkset-json format (#882)

--- src/app/features/files/pages/file-detail/file-detail.component.spec.ts
+++ src/app/features/files/pages/file-detail/file-detail.component.spec.ts
@@ -104,7 +104,7 @@ describe('FileDetailComponent', () => {
     expect(linkTags.length).toBe(2);
     expect(linkTags[0].getAttribute('href')).toBe('http://localhost:4200/metadata/file-1/?format=linkset');
     expect(linkTags[0].getAttribute('type')).toBe('application/linkset');
-    expect(linkTags[1].getAttribute('href')).toBe('http://localhost:4200/metadata/file-1/?format=linkset%2Bjson');
+    expect(linkTags[1].getAttribute('href')).toBe('http://localhost:4200/metadata/file-1/?format=linkset-json');
     expect(linkTags[1].getAttribute('type')).toBe('application/linkset+json');
   });
 });
--- src/app/features/registry/pages/registry-overview/registry-overview.component.spec.ts
+++ src/app/features/registry/pages/registry-overview/registry-overview.component.spec.ts
@@ -645,7 +645,7 @@ describe('RegistryOverviewComponent', () => {
       expect(linkTags.length).toBe(2);
       expect(linkTags[0].getAttribute('href')).toBe('http://localhost:4200/metadata/registry-1/?format=linkset');
       expect(linkTags[0].getAttribute('type')).toBe('application/linkset');
-      expect(linkTags[1].getAttribute('href')).toBe('http://localhost:4200/metadata/registry-1/?format=linkset%2Bjson');
+      expect(linkTags[1].getAttribute('href')).toBe('http://localhost:4200/metadata/registry-1/?format=linkset-json');
       expect(linkTags[1].getAttribute('type')).toBe('application/linkset+json');
     });
 
